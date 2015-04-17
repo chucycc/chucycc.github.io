e92f2bcd37746f1c905422a0f189d8afd1767657
chucycc <mf27262000@gmail.com>
2015-04-17 12:44:30 -0400

    Adjust zoom-in dot size

--- js/d3Map.js
+++ js/d3Map.js
@@ -15,9 +15,13 @@
       .domain([20000, 200000])
       .range(["yellow", "green"]);
 
-    var zoom = d3.behavior.zoom()
-      .scaleExtent([1, 30])
-      .on("zoom", zoomed);
+	var X = d3.scale.linear()
+      .domain([0,width])
+	  .range([0,width])
+	var Y = d3.scale.linear()
+      .domain([0,height])
+      .range([0,height]);
+
 
     var path = d3.geo.path()
       .projection(projection)
@@ -38,9 +42,7 @@
       .on('mouseover', reset);
 
     var g = svg.append("g")
-    svg
-    //.call(zoom) // delete this line to disable free zooming
-    .call(zoom.event);
+
 
     /*Zip infobox*/
     var div = bar
@@ -74,7 +76,14 @@
     .defer(d3.csv, "data.csv")
     .await(ready);
 
+
 function ready(error, us, hs) {
+    var zoom = d3.behavior.zoom()
+      .x(X)
+      .y(Y)
+      .scaleExtent([1, 30])
+      .on("zoom", zoomed);
+
     var zip = topojson.feature(us, us.objects.tl_2014_us_zcta510);
 
 	var obj_names = Object.keys(hs[0]).slice(7,17);
@@ -83,7 +92,7 @@ function ready(error, us, hs) {
 	});
 
 // zip code region
-    g.selectAll(".zip")
+    var zipregion = g.selectAll(".zip")
     .data(zip.features)
     .enter().append("path")
     .attr("class", "zip")
@@ -93,14 +102,14 @@ function ready(error, us, hs) {
     .on('mouseover', mouseover);
 
 // state-boundary
-    g.append("path")
+    var statebound = g.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) {
         return a !== b;}))
       .attr("class", "state-boundary")
         .attr("d", path);
 
 // land-boundary
-    g.append("path")
+    var landbound = g.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) {
         return a === b && a.id !== 2 && a.id !== 15;}))
     .attr("class", "land-boundary")
@@ -108,17 +117,12 @@ function ready(error, us, hs) {
 
 // hospital dots
     var active = false;
-    g.selectAll("circle")
+    var circle = g.selectAll("circle")
      .data(hs)
      .enter()
      .append("circle")
-     .attr("cx", function(d) {
-        return projection([d.lng, d.lat])[0];
-     })
-     .attr("cy", function(d) {
-        return projection([d.lng, d.lat])[1];
-     })
      .attr("r", 1.8)
+     .attr("transform", transform)
      .attr("class", "hospital")
       .on('mouseover', mouseoverHS)
       .on('mouseout', mouseoutHS)
@@ -129,11 +133,22 @@ function ready(error, us, hs) {
     .attr("y", height * 0.1)
     /*.text("hello");*/
 
+    svg
+    .call(zoom) // delete this line to disable free zooming
+    .call(zoom.event);
+
+    function zoomed() {
+	statebound.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  	landbound.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  	zipregion.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+  	circle.attr("transform", transform);
+	}
+  	function transform(d) {
+  	  return "translate(" + X(projection([d.lng, d.lat])[0]) + "," + Y(projection([d.lng, d.lat])[1]) + ")";
+	}
 };
 
-function zoomed() {
-  	g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
+
 
 function mouseover(d) {
  	 content
